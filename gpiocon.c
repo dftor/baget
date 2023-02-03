6baaa93a1ce9f8376eec9e92435c76fa1e38de9f
dftor <dftor-dev@mail.ru>
2023-02-03 16:21:43 +0300

    Edit files

--- gpiocon.c
+++ gpiocon.c
@@ -1,1 +1,30 @@
 #include "gpiocon.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int gpiocon_export(int gpio_index) {
+	FILE *export = fopen(EXPORT, "w");
+	if (export == NULL) {
+		return 1;
+	}
+	fprintf(export, "%d", gpio_index);
+	fclose(export);
+	return 0;
+}
+
+int gpiocon_export(int gpio_index) {
+	FILE *unexport = fopen(UNEXPORT, "w");
+	if (unexport == NULL) {
+		return 1;
+	}
+	fprintf(unexport, "%d", gpio_index);
+	fclose(unexport);
+	return 0;
+}
+
+int gpiocon_set_direction(int gpio_index, const char *mode) {
+	
+	
+}
--- gpiocon.h
+++ gpiocon.h
@@ -8,9 +8,12 @@
 #define LOW 0
 #define HIGH 1
 
+#define INPUT "in"
+#define OUTPUT "out"
+
 int gpiocon_export(int gpio_index);
 int gpiocon_unexport(int gpio_index);
-int gpiocon_set_direction(int gpio_index);
+int gpiocon_set_direction(int gpio_index, const char *mode);
 int gpiocon_set_value(int gpio_index);
 
 #endif
