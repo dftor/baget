53224c45a4a2a189e415e01b995652258e7d3885
dftor <dftor-dev@mail.ru>
2023-02-03 17:01:24 +0300

    Edit files

--- gpiocon.c
+++ gpiocon.c
@@ -1,3 +1,12 @@
+#define GPIO_ROOT "sys/class/gpio"
+#define GPIO_EXPORT "sys/class/gpio/export"
+#define GPIO_UNEXPORT "sys/class/gpio/unexport"
+
+#define LOW 0
+#define HIGH 1
+#define INPUT "in"
+#define OUTPUT "out"
+
 #include "gpiocon.h"
 
 #include <stdio.h>
@@ -5,26 +14,49 @@
 #include <unistd.h>
 
 int gpiocon_export(int gpio_index) {
-	FILE *export = fopen(EXPORT, "w");
-	if (export == NULL) {
+	FILE *export_stream = fopen(GPIO_EXPORT, "w");
+	if (export_stream == NULL) {
 		return 1;
 	}
-	fprintf(export, "%d", gpio_index);
-	fclose(export);
+	fprintf(export_stream, "%d", gpio_index);
+	fclose(export_stream);
 	return 0;
 }
 
-int gpiocon_export(int gpio_index) {
-	FILE *unexport = fopen(UNEXPORT, "w");
-	if (unexport == NULL) {
+int gpiocon_unexport(int gpio_index) {
+	FILE *unexport_stream = fopen(GPIO_UNEXPORT, "w");
+	if (unexport_stream == NULL) {
 		return 1;
 	}
-	fprintf(unexport, "%d", gpio_index);
-	fclose(unexport);
+	fprintf(unexport_stream, "%d", gpio_index);
+	fclose(unexport_stream);
 	return 0;
 }
 
 int gpiocon_set_direction(int gpio_index, const char *mode) {
-	
-	
+	char direction_path[256];
+	sprintf(direction_path,
+			"sys/class/gpio/gpio%d/direction",
+			gpio_index);
+	FILE *direction_stream = fopen(direction_path, "w");
+	if (direction_stream == NULL) {
+		return 1;
+	}
+	fprintf(direction_stream, "%s", mode);
+	fclose(direction_stream);
+	return 0;
+}
+
+int gpiocon_set_value(int gpio_index, int value) {
+	char value_path[256];
+	sprintf(value_path,
+			"sys/class/gpio/gpio%d/value",
+			gpio_index);
+	FILE *value_stream = fopen(value_path, "w");
+	if (value_stream == NULL) {
+		return 1;
+	}
+	fprintf(value_stream, "%d", value);
+	fclose(value_stream);
+	return 0;
 }
--- gpiocon.h
+++ gpiocon.h
@@ -1,19 +1,9 @@
 #ifndef GPIOCON_H
 #define GPIOCON_H
 
-#define GPIO_ROOT "sys/class/gpio"
-#define GPIO_EXPORT "sys/class/gpio/export"
-#define GPIO_UNEXPORT "sys/class/gpio/unexport"
-
-#define LOW 0
-#define HIGH 1
-
-#define INPUT "in"
-#define OUTPUT "out"
-
 int gpiocon_export(int gpio_index);
 int gpiocon_unexport(int gpio_index);
 int gpiocon_set_direction(int gpio_index, const char *mode);
-int gpiocon_set_value(int gpio_index);
+int gpiocon_set_value(int gpio_index, int value);
 
 #endif
