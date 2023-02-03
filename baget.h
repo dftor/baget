1b1418aab2bc0af8ad9efd57d327943bbafab471
dftor <dftor-dev@mail.ru>
2023-02-03 14:14:55 +0300

    Edit files

--- baget.h
+++ baget.h
@@ -1,6 +1,16 @@
 #ifndef BAGET_H
 #define BAGET_H
 
+const char GPIO_ROOT[] = "sys/class/gpio";
+const char GPIO_EXPORT[] = "sys/class/gpio/export";
+const char GPIO_UNEXPORT[] = "sys/class/gpio/unexport";
 
+bool LOW = 0;
+bool HIGH = 1;
+
+bool INPUT = 0;
+bool OUTPUT = 1;
+
+void pinMode(uint8_t pin, bool mode);
 
 #endif
