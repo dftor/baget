a6f4d7c835afb3fb1a29884ae332f5e13d95fb35
dftor <dftor-dev@mail.ru>
2023-02-06 16:39:16 +0300

    Added error handling via perror()

--- gpiocon.c
+++ gpiocon.c
@@ -1,62 +1,53 @@
-#define GPIO_ROOT "sys/class/gpio"
-#define GPIO_EXPORT "sys/class/gpio/export"
-#define GPIO_UNEXPORT "sys/class/gpio/unexport"
-
-#define LOW 0
-#define HIGH 1
-#define INPUT "in"
-#define OUTPUT "out"
-
 #include "gpiocon.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int gpiocon_export(int gpio_index) {
-	FILE *export_stream = fopen(GPIO_EXPORT, "w");
-	if (export_stream == NULL) {
-		return 1;
+void gpiocon_export(int gpio_index) {
+	FILE *export_ptr = fopen(GPIO_EXPORT, "w");
+	if (export_ptr == NULL) {
+		perror("Eport error");
+		exit(EXIT_FAILURE);
 	}
-	fprintf(export_stream, "%d", gpio_index);
-	fclose(export_stream);
-	return 0;
+	fprintf(export_ptr, "%d", gpio_index);
+	fclose(export_ptr);
 }
 
-int gpiocon_unexport(int gpio_index) {
-	FILE *unexport_stream = fopen(GPIO_UNEXPORT, "w");
-	if (unexport_stream == NULL) {
-		return 1;
+void gpiocon_unexport(int gpio_index) {
+	FILE *unexport_ptr = fopen(GPIO_UNEXPORT, "w");
+	if (unexport_ptr == NULL) {
+		perror("Unexport error");
+		exit(EXIT_FAILURE);
 	}
-	fprintf(unexport_stream, "%d", gpio_index);
-	fclose(unexport_stream);
-	return 0;
+	fprintf(unexport_ptr, "%d", gpio_index);
+	fclose(unexport_ptr);
 }
 
-int gpiocon_set_direction(int gpio_index, const char *mode) {
+void gpiocon_set_direction(int gpio_index, const char *mode) {
 	char direction_path[256];
 	sprintf(direction_path,
 			"sys/class/gpio/gpio%d/direction",
 			gpio_index);
-	FILE *direction_stream = fopen(direction_path, "w");
-	if (direction_stream == NULL) {
-		return 1;
+	FILE *direction_ptr = fopen(direction_path, "w");
+	if (direction_ptr == NULL) {
+		perror("Set direction error");
+		exit(EXIT_FAILURE);
 	}
-	fprintf(direction_stream, "%s", mode);
-	fclose(direction_stream);
-	return 0;
+	fprintf(direction_ptr, "%s", mode);
+	fclose(direction_ptr);
 }
 
-int gpiocon_set_value(int gpio_index, int value) {
+void gpiocon_set_value(int gpio_index, int value) {
 	char value_path[256];
 	sprintf(value_path,
 			"sys/class/gpio/gpio%d/value",
 			gpio_index);
-	FILE *value_stream = fopen(value_path, "w");
-	if (value_stream == NULL) {
-		return 1;
+	FILE *value_ptr = fopen(value_path, "w");
+	if (value_ptr == NULL) {
+		perror("Set value error");
+		exit(EXIT_FAILURE);
 	}
-	fprintf(value_stream, "%d", value);
-	fclose(value_stream);
-	return 0;
+	fprintf(value_ptr, "%d", value);
+	fclose(value_ptr);
 }
--- gpiocon.h
+++ gpiocon.h
@@ -1,9 +1,13 @@
 #ifndef GPIOCON_H
 #define GPIOCON_H
 
-int gpiocon_export(int gpio_index);
-int gpiocon_unexport(int gpio_index);
-int gpiocon_set_direction(int gpio_index, const char *mode);
-int gpiocon_set_value(int gpio_index, int value);
+#define GPIO_ROOT "sys/class/gpio"
+#define GPIO_EXPORT "sys/class/gpio/export"
+#define GPIO_UNEXPORT "sys/class/gpio/unexport"
+
+void gpiocon_export(int gpio_index);
+void gpiocon_unexport(int gpio_index);
+void gpiocon_set_direction(int gpio_index, const char *mode);
+void gpiocon_set_value(int gpio_index, int value);
 
 #endif
