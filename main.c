48fe1c77f68cab044111d7b03b98f8ba738c4dbc
dftor <dftor-dev@mail.ru>
2023-02-07 18:16:32 +0300

    Added main.c to control led blinks

--- main.c
+++ main.c
@@ -1,11 +1,32 @@
 #include "baget.h"
 
+#include <dos.h>
+
 int main() {
-	const char *PIN_LED = "A4";
-	const char *PIN_AMOGUS = "R10";
+	const char *LED_GREEN = "C7";
+	const char *LED_YELLOW = "A7";
+	const char *LED_RED = "B3";
 	
-	pinMode(PIN_LED, OUTPUT);
-	digitalWrite(PIN_LED, HIGH);
+	pinMode(LED_GREEN, OUTPUT);
+	pinMode(LED_YELLOW, OUTPUT);
+	pinMode(LED_RED, OUTPUT);
+	digitalWrite(LED_GREEN, LOW);
+	digitalWrite(LED_YELLOW, LOW);
+	digitalWrite(LED_RED, LOW);
+	
+	for (int i = 0; i < 5; i++) {
+		digitalWrite(LED_GREEN, HIGH);
+		delay(500);
+		digitalWrite(LED_GREEN, LOW);
+		
+		digitalWrite(LED_YELLOW, HIGH);
+		delay(500);
+		digitalWrite(LED_YELLOW, LOW);
+		
+		digitalWrite(LED_RED, HIGH);
+		delay(500);
+		digitalWrite(LED_RED, LOW);
+	}
 	
 	return 0;
 }
