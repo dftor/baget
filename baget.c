75ee6f9d308bc119809bc5b7a0b814c64d2a422f
dftor <dftor-dev@mail.ru>
2023-02-07 17:18:26 +0300

    Added cosmetic fixes

--- baget.c
+++ baget.c
@@ -18,14 +18,14 @@ void pinMode(const char *pin, const char *mode) {
 	int gpiochip = pin_to_gpiochip(pin);
 	int export_returned = gpiocon_export(gpiochip);
 	if (export_returned == -1) {
-		printf("EXPORT ERROR: Unable to export %s pin!\n", pin);
+		printf("EXPORT_ERROR: Unable to export %s pin!\n", pin);
 		print_error_termination_message();
 		exit(EXIT_FAILURE);
 	}
 	
 	int direction_returned = gpiocon_set_direction(gpiochip, mode);
 	if (direction_returned == -1) {
-		printf("SET DIRECTION ERROR: Unable to set %s direction of %s pin!\n", mode, pin);
+		printf("SET_DIRECTION_ERROR: Unable to set %s direction of %s pin!\n", mode, pin);
 		print_error_termination_message();
 		exit(EXIT_FAILURE);
 	}
@@ -35,7 +35,7 @@ void digitalWrite(const char *pin, int value) {
 	int gpiochip = pin_to_gpiochip(pin);
 	int set_value_returned = gpiocon_set_value(gpiochip, value);
 	if (set_value_returned == -1) {
-		printf("SET VALUE ERROR: Unable to set %d of %s pin!\n", value, pin);
+		printf("SET_VALUE_ERROR: Unable to set %d of %s pin!\n", value, pin);
 		print_error_termination_message();
 		exit(EXIT_FAILURE);
 	}
