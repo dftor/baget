7af71105bc3548cc9a2b4f63287c9106980858e6
dftor <dftor-dev@mail.ru>
2023-02-15 16:07:31 +0300

    Added some fixes

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
 #include "button.h"
 
+#include <stdio.h>
+#include <time.h>
+
 int main() {
-	Button left = init("F1", "LEFT", 10);
+	Button left = init("D2", "LEFT", 10);
+	Button middle = init("B3", "MIDDLE", 10);
+	Button right = init("C6", "RIGHT", 10);
 	
-	printStateMessage(&left);
+	printf("Button status tracking has started!\n");
+	
+	int trackingTime = 0;
+	printf("Input time of tracking as seconds:\n");
+	scanf("%d", &trackingTime);
+	
+	time_t begin = time(NULL);
+	time_t end = begin;
+	
+	while (end - begin < trackingTime) {
+		end = time(NULL);
+		
+		checkState(&left);
+		if (left.isNewState == 1) printStateMessage(&left);
+		
+		checkState(&middle);
+		if (middle.isNewState == 1) printStateMessage(&middle);
+		
+		checkState(&right);
+		if (right.isNewState == 1) printStateMessage(&right);
+	}
 	
 	clear(&left);
-
+	clear(&middle);
+	clear(&right);
+	
+	printf("The program terminated successfully.\n");
 	
 	return 0;
 }
